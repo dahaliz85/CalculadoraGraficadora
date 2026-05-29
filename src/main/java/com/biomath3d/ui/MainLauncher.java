728bf3a666dac5d5452e4ffc981b4f2e677416fd
dahaliz85 <dahaliz85@gmail.com>
2026-05-28 21:19:53 -0600

    reinicio de codigo

--- src/main/java/com/biomath3d/ui/MainLauncher.java
+++
@@ -1,8 +0,0 @@
-package com.biomath3d.ui;
-
-public class MainLauncher {
-
-    public static void main(String[] args) {
-        MainApp.main(args);
-    }
-}
--- src/main/java/com/biomath3d/util/Constants.java
+++
@@ -1,8 +0,0 @@
-package com.biomath3d.util;
-
-public class Constants {
-
-    public static final String DATABASE_DRIVER = "jdbc:sqlite:biomath3d.db";
-
-
-}
