f0e36c6eec873029763806730ca2647347f1e8be
dahaliz85 <dahaliz85@gmail.com>
2026-05-29 00:10:02 -0600

    reinicio de codigo

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -10,9 +10,11 @@ module com.biomath3d {
     // 2. Exportamos el paquete raíz para que JavaFX pueda lanzar MainApp
     exports com.biomath3d;
     exports com.biomath3d.controller;
+    exports com.biomath3d.utils;
 
     // 3. ¡CRUCIAL! Abrimos el paquete a javafx.fxml para que el FXMLLoader
     // pueda leer e inyectar campos en tu MainController de forma reflexiva.
     opens com.biomath3d to javafx.fxml;
     opens com.biomath3d.controller to javafx.fxml;
+    opens com.biomath3d.utils to javafx.fxml;
 }
