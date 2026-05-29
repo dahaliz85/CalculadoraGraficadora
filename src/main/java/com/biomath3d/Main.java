46356a8f7742590c70dd57ce6b8c340b78b3fa67
dahaliz85 <dahaliz85@gmail.com>
2026-05-28 22:24:58 -0600

    reinicio de codigo

--- src/main/java/com/biomath3d/Main.java
+++ src/main/java/com/biomath3d/Main.java
@@ -6,18 +6,34 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class Main extends Application {
+
+    private double xOffset = 0;
+    private double yOffset = 0;
 
     @Override
     public void start(Stage primaryStage) throws Exception {
         // Apunta correctamente a la carpeta ui exclusiva de resources
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/com.biomath3d.ui.view/MainView.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/biomath3d/ui/view/MainView.fxml"));
         Parent root = loader.load();
 
         Scene scene = new Scene(root, 1200, 800);
         primaryStage.setTitle("BioMath 3D - Calculadora Gráfica");
         primaryStage.setScene(scene);
+
+        primaryStage.initStyle(StageStyle.UNDECORATED);
+
+        root.setOnMousePressed(event -> {
+            xOffset = event.getSceneX();
+            yOffset = event.getSceneY();
+        });
+
+        root.setOnMouseDragged(event -> {
+            primaryStage.setX(event.getScreenX() - xOffset);
+            primaryStage.setY(event.getScreenY() - yOffset);
+        });
 
         // Carga el icono desde la ubicación exclusiva de resources
         try {
@@ -26,6 +42,7 @@ public class Main extends Application {
             System.out.println("Nota: Icono no encontrado.");
         }
 
+        primaryStage.initStyle(javafx.stage.StageStyle.UNDECORATED);
         primaryStage.show();
     }
 
